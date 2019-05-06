4d6f965033440a869b6ac7a9bdd54f457c4e6fe8
Richard Wang <rwang83@gmail.com>
2019-05-06 03:49:18 -0400

    update tr grep

--- waterbill/run.sh
+++ waterbill/run.sh
@@ -7,9 +7,9 @@ curl -v -b out/cookiejar.tmp "https://old.bwsc.org/ACCOUNTS/trans_history.asp" >
 cat out/html.tmp \
 | tr -d "\n\r" `# make into single line`\
 | tr -s " "    `# merge multiple spaces`\
-| grep -Po "<tr>.*?</tr>"  `# get the tr's`\
-| tail -n +3 `# remove top 3 lines`\
-| tr -d "<tdr/>$" `# remove unwanted chars` \
+| grep -Po "<tr.*?>.*?</tr>"  `# get the tr's`\
+| tail -n +3 `# remove top lines`\
+| tr -d "<tdr/>class=od\"$" `# remove unwanted chars` \
 | tr -s " " `# merge multiple spaces (again)`\
 | awk '{ print substr($1,5,4)"/"substr($1,1,2)"/"substr($1,3,2), $2, $3 + $4 }' `# date, consumption, cost`\
 | sort -n `# sort number` \
