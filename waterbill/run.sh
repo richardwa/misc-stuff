8b39d695a9d553a6eaca029100aed8e375b56a8b
Richard Wang <rwang83@gmail.com>
2019-05-06 04:26:29 -0400

    fixed

--- waterbill/run.sh
+++ waterbill/run.sh
@@ -7,12 +7,12 @@ curl -v -b out/cookiejar.tmp "https://old.bwsc.org/ACCOUNTS/trans_history.asp" >
 cat out/html.tmp \
 | tr -d "\n\r" `# make into single line`\
 | tr -s " "    `# merge multiple spaces`\
-| grep -Po "<tr>.*?</tr>"  `# get the tr's`\
+| grep -Po "<tr.*?>.*?</tr>"  `# get the tr's`\
 | tail -n +3 `# remove top 3 lines`\
-| tr -d "<tdr/>$" `# remove unwanted chars` \
+| tr -d "<tdr/>clas=\"od$" `# remove unwanted chars` \
 | tr -s " " `# merge multiple spaces (again)`\
-| awk '{ print substr($1,5,4)"/"substr($1,1,2)"/"substr($1,3,2), $2, $3 + $4 }' `# date, consumption, cost`\
+| awk '{ print substr($1,5,4)"/"substr($1,1,2)"/"substr($1,3,2), $2, $3 + $4 }' `# bill date, consumption, sum costs`\
 | sort -n `# sort number` \
 > out/formatted.txt
 
-echo "date consumption cost" | cat - out/formatted.txt
+echo "billdate consumption charges" | cat - out/formatted.txt
