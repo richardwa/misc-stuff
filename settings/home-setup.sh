3b86b83968796daa5b66a10ff32f6f1d22338303
rich <rwang83@gmail.com>
2020-02-27 18:01:24 -0500

    update

--- settings/home-setup.sh
+++ settings/home-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo $DIR
+ln -sf "$DIR" ~/.settings
 ln -sf "$DIR/dotprofile" ~/.profile
 ln -sf "$DIR/dotbash_profile" ~/.bash_profile
 ln -sf "$DIR/dotbash_profile" ~/.bashrc
