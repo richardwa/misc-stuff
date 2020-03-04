6e1755ba1e3138c46aa44678a4a6b1dcc7d3b414
Richard Wang <rwang83@gmail.com>
2020-03-04 02:21:49 -0500

    update

--- settings/home-setup.sh
+++ settings/home-setup.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo $DIR
+
+# settings
 ln -sf "$DIR" ~/.settings
 ln -sf "$DIR/dotprofile" ~/.profile
 ln -sf "$DIR/dotbash_profile" ~/.bash_profile
 ln -sf "$DIR/dotbash_profile" ~/.bashrc
 ln -sf "$DIR/dottmux.conf" ~/.tmux.conf
 
-#vim stuff
+# home
+ln -sf "/mnt/h/Documents" ~/Documents
+ln -sf "/mnt/h/3D" ~/3D
+ln -sf "/mnt/c/Users/rwang/Downloads" ~/Downloads
+
+# vim stuff
 mkdir -p ~/.vim/bundle
 mkdir -p ~/.config
 ln -sf ~/.vim ~/.config/nvim
