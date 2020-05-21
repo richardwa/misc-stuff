2b336f7885fdc5125af5a845ee79e6f204cfbdf3
rich <rwang83@gmail.com>
2020-05-21 03:54:16 -0400

    update

--- settings/home-setup.sh
+++ settings/home-setup.sh
@@ -3,21 +3,20 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo $DIR
 
 # settings
-ln -sf "$DIR/dotprofile" ~/.profile
-ln -sf "$DIR/dotprofile" ~/.bash_profile
-ln -sf "$DIR/dotprofile" ~/.bashrc
-# ln -sf "$DIR/dottmux.conf" ~/.tmux.conf
-
-# home
-ln -sf "/mnt/h/Documents" ~/Documents
-ln -sf "/mnt/h/3D" ~/3D
-ln -sf "/mnt/c/Users/rwang/Downloads" ~/Downloads
+if grep -q dotprofile ~/.bashrc; then
+  echo profile already added
+else
+  echo source $DIR/dotprofile >> ~/.bashrc
+fi
 
 # vim stuff
 mkdir -p ~/.vim/bundle
+ln -sf "$DIR/dotvimrc" ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# copy vim config to nvm
 mkdir -p ~/.config
 ln -sf ~/.vim ~/.config/nvim
-ln -sf "$DIR/dotvimrc" ~/.vimrc
 ln -sf "$DIR/dotvimrc" ~/.config/nvim/init.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 
