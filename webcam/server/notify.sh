3d330213813388630c5e47d666ee5000cba3d613
Richard Wang <rwang83@gmail.com>
2020-06-02 23:19:14 -0400

    update

--- webcam/server/notify.sh
+++ webcam/server/notify.sh
@@ -1,12 +1,12 @@
 
 #Detect the name of the display in use
-display=":$(ls /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | head -n 1)"
+tty=`cat /sys/class/tty/tty0/active`
 
 #Detect the user using such display
-user=$(who | grep '('$display')' | awk '{print $1}' | head -n 1)
+user=$(who | grep $tty | awk '{print $1}' | head -n 1)
 
 #Detect the id of the user
 uid=$(id -u $user)
 
-echo $display, $user, $uid
-#sudo -u $user DISPLAY=$display DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$uid/bus notify-send "$@"
+echo $tty, $user, $uid
+sudo -u $user DISPLAY=$display DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$uid/bus notify-send "$@"
