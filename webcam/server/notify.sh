0016dfc11ec57cfe70d18a9aa3c6bbd3b428848c
Richard Wang <rwang83@gmail.com>
2020-06-03 22:56:48 -0400

    update

--- webcam/server/notify.sh
+++ webcam/server/notify.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
 
-#Detect the name of the display in use
+# find current tty
 tty=`cat /sys/class/tty/tty0/active`
 
-#Detect the user using such display
+# find the user of the tty
 user=$(who | grep $tty | awk '{print $1}' | head -n 1)
+
+# get display of user
+display=`ps -fu $user | grep -o "/etc/X11/xinit/xserverrc :." | cut -d: -f2`
 
 #Detect the id of the user
 uid=$(id -u $user)
 
-echo $tty, $user, $uid
+echo $tty, $user, $display, $uid
+
 sudo -u $user DISPLAY=$display DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$uid/bus notify-send "$@"
