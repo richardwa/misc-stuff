e41ca786691c92be6ef3db62a9cba88b33e216a2
Richard Wang <rwang83@gmail.com>
2020-10-05 04:21:50 -0400

    add data manager

--- push-to-talk-google-meet/bookmarklet.js
+++
@@ -1,6 +0,0 @@
- (() => {
-  const toggle = tip => ({ key }) =>
-    key === 'Control' && document.querySelectorAll(`[data-tooltip*='${tip}']`).click();
-  document.body.onkeyup = toggle('Turn off microphone');
-  document.body.onkeydown = toggle('Turn on microphone');
-})();
--- push-to-talk-google-meet/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-PWD=`pwd`
-
-cd $DIR
-rm pushtotalk.zip
-zip pushtotalk.zip * 
-cd $PWD
