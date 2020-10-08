2ba5c11029df992a45cd563cddab60a64d7446ee
Richard Wang <rwang83@gmail.com>
2020-10-07 20:37:11 -0400

    update

--- backup-sync/sync.sh
+++ backup-sync/sync.sh
@@ -6,4 +6,3 @@ rsync -tav $SOURCE/Pictures/* $TARGET/pictures
 rsync -tav $SOURCE/Music/* $TARGET/music
 rsync -tav $SOURCE/Videos/* $TARGET/videos
 rsync -tav $SOURCE/Roms/* $TARGET/roms
-rsync -tav $SOURCE/LaunchBox/* $TARGET/LaunchBox
