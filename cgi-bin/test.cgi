6be304ecdd1e55a03fb26bd2170173b94760f800
Richard Wang <rwang83@gmail.com>
2020-10-10 04:05:59 -0400

    clean up

--- cgi-bin/test.cgi
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "content-type: text/plain"
-echo ""
-echo "$QUERY_STRING"
-if [[ "${QUERY_STRING,,}" == *.jpg ]]; then
-    echo "true"
-else
-    echo "false"
-fi
--- pwsh/popup.ps1
+++
@@ -1,9 +0,0 @@
-
-$balloon = New-Object System.Windows.Forms.NotifyIcon
-$path = (Get-Process -id $pid).Path
-$balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path) 
-$balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info 
-$balloon.BalloonTipText = 'What do you think of this balloon tip?'
-$balloon.BalloonTipTitle = "Attention $Env:USERNAME" 
-$balloon.Visible = $true 
-$balloon.ShowBalloonTip(50000)
