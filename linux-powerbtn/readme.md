aea12e2235874bd0b4dc4d6f2fe0f0e3676e6253
Richard Wang <rwang83@gmail.com>
2020-10-10 03:51:51 -0400

    update

--- linux-powerbtn/readme.md
+++ linux-powerbtn/readme.md
@@ -1,2 +1,4 @@
 # allow power button to turn monitor on and off
 
+disable power button in gnome settings (set to Do Nothing)
+use acpi_listen to figure out event names
--- linux-powerbtn/root/etc/acpi/power-button.sh
+++ linux-powerbtn/root/etc/acpi/power-button.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 # this files runs as root when triggered by acpi events
-echo power button pressed >> /tmp/power.log
 
-display=DP-0
+echo "args: $@" >> /tmp/power.log
+export XAUTHORITY=/run/user/1000/gdm/Xauthority
+export DISPLAY=:2
+MONITOR=DP-0 
 
-if xrandr | grep "$display disconnected"; then
-    xrandr --output "$display" --auto
-else
-    xrandr --output "$display" --off 
-fi
+function toggle() {
+    if xrandr --listmonitors | grep "$MONITOR"; then
+        echo power off
+        xrandr --output "$MONITOR" --off
+    else
+        echo power on
+        xrandr --output "$MONITOR" --auto
+    fi
+}
+
+# disable for now, turning back on does not work
+# toggle |& tee -a /tmp/power.log
