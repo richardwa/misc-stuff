4e30a5b5e5933c1be96ffe1bdb7da103609f612c
Richard Wang <rwang83@gmail.com>
2020-10-11 23:33:38 -0400

    update

--- tuya-config/get-data.sh
+++ tuya-config/get-data.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-LOG=$DIR/out/out.log
 for  i in 1 2 3
 do
-  curl -s http://powermon${i}/cm?cmnd=Status%208  -w "\n" | tee -a $LOG | jq "{
+  curl -s http://powermon${i}/cm?cmnd=Status%208  -w "\n" | jq "{
     id: ${i}, 
     power_watts: .StatusSNS.ENERGY.Power, 
     total_kwh: .StatusSNS.ENERGY.Total,
     time:  .StatusSNS.ENERGY.TotalStartTime,
   }"
 done
-echo "" >> $LOG
