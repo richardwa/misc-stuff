c8d416f24d905c8419954b6cd2dfa75c3698450f
Richard Wang <rwang83@gmail.com>
2020-10-17 22:46:20 -0400

    simplify udev rules

---
+++ linux-multiseat/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# do dry run
+args="-nrv"
+
+if [[ $EUID -eq 0 ]]; then
+    # is root user execute for real
+    args="-rv"
+    rm /etc/udev/rules.d/72-*
+fi
+rsync $args $DIR/root/* /
