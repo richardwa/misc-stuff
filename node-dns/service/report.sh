c8f0d6ea82a41fc2baf1d08481b0679130033e61
rich <rwang83@gmail.com>
2021-04-17 22:08:26 -0400

    update reports

--- node-dns/service/report.sh
+++ node-dns/service/report.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
 # requests per device
-# journalctl -u node-dns --no-pager --since "1 day ago" | awk '{if ($7 ~ "192.168") a[$7]++;}END{for(i in a)printf "%-15s %d\n", i,a[i]}'
+printf "\n\nCLIENT REQUESTS\n"
+journalctl -u node-dns --no-pager --since "1 day ago" -o short-iso |
+awk 'NR==1 {print "req allow client site domain"}; NR!= 1 {n=split($6,b,"."); print substr($1,1,19),$4,$5,$6,b[n-1]"."b[n]}' |
+textql --header --output-header -dlm " " -sql " 
+select client, count(1) as count
+group by client
+order by count
+" | awk -F, 'NR != 1 { n=int(50 * $2/5000); printf("%-20.20s %5d ",$1,$2); while (n--) printf("*"); print("") }'
 
+printf "\n\nDOMAIN REQUESTS\n"
+journalctl -u node-dns --no-pager --since "1 day ago" -o short-iso |
+awk 'NR==1 {print "req allow client site domain"}; NR!= 1 {n=split($6,b,"."); print substr($1,1,19),$4,$5,$6,b[n-1]"."b[n]}' |
+textql --header --output-header -dlm " " -sql " 
+select domain, count(1) as count
+group by domain
+order by count desc
+limit 50
+" | awk -F, 'NR != 1 { n=int(50 * $2/5000); printf("%-20.20s %5d ",$1,$2); while (n--) printf("*"); print("") }'
 
-# per device details
-# journalctl -u node-dns --no-pager --since "1 day ago" | 
-#   awk '{ if ($7 ~ "192.168") {n=split($8,b,"."); a[$7" "$6" "b[n-1]"."b[n]]++;}}END{for(i in a) print i,a[i]}' |
-#   awk '{ printf "%-15s %05d %s %s\n", $1, $4, $2, $3 }' | 
-#   sort 
-
+printf "\n\nACTIVITY BY HOUR\n"
 journalctl -u node-dns --no-pager --since "1 day ago" -o short-iso |
 awk 'NR==1 {print "req allow client site domain"}; NR!= 1 {n=split($6,b,"."); print substr($1,1,19),$4,$5,$6,b[n-1]"."b[n]}' |
 textql --header --output-header -dlm " " -sql " 
 select strftime('%Y-%m-%d %H',req) as _date, count(1) as count
 group by _date
-" | awk -F, 'NR != 1 { n=int(50 * $2/5000); printf("%s %5d ",$1,$2); while (n--) printf("*"); print("") }'
+" | awk -F, 'NR != 1 { n=int(50 * $2/5000); printf("%-20.20s %5d ",$1,$2); while (n--) printf("*"); print("") }'
