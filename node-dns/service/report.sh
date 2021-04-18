62365d85eafdbf653cf28228be695da2c246616f
rich <rwang83@gmail.com>
2021-04-17 22:32:21 -0400

    update

--- node-dns/service/report.sh
+++ node-dns/service/report.sh
@@ -10,7 +10,7 @@ group by client
 order by count
 " | awk -F, 'NR != 1 { n=int(50 * $2/5000); printf("%-20.20s %5d ",$1,$2); while (n--) printf("*"); print("") }'
 
-printf "\n\nDOMAIN REQUESTS\n"
+printf "\n\nTOP 50 DOMAIN REQUESTS\n"
 journalctl -u node-dns --no-pager --since "1 day ago" -o short-iso |
 awk 'NR==1 {print "req allow client site domain"}; NR!= 1 {n=split($6,b,"."); print substr($1,1,19),$4,$5,$6,b[n-1]"."b[n]}' |
 textql --header --output-header -dlm " " -sql " 
