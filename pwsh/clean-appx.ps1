ef4704d7f3affbbad7322e3ce831151da12be3c9
Richard Wang <rwang83@gmail.com>
2021-04-21 18:34:07 -0400

    update

---
+++ pwsh/clean-appx.ps1
@@ -0,0 +1,16 @@
+$whitelist = @(
+  "9E2F88E3.Twitter",
+  "89006A2E.AutodeskSketchBook",
+  "PandoraMediaInc.29680B314EFC2",
+  "4DF9E0F8.Netflix",
+  "6918E89D.TheOthello",
+  "OutOfTheBitLtd.FourInARow2014",
+  "SpotifyAB.SpotifyMusic",
+  "DellPrinter.DellDocumentHub",
+  "D50536CD.CitrixReceiver"
+)
+
+
+Get-AppxPackage -User albert | Where-Object {
+  !$_.Publisher.contains('O=Microsoft Corporation,') -and !$whitelist.Contains($_.name)
+} | Remove-AppxPackage -AllUsers
